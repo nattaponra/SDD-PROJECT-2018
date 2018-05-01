397c86b535da59a6127893755a039b8b628c929d
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-05-01 12:17:30 +0700

    Create profile submission

--- Project/www/app/Http/Controllers/UserController.php
+++ Project/www/app/Http/Controllers/UserController.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProfileRequest;
+use App\Province;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
 
+    private $provinceModel;
+    private $userModel;
+    public function __construct(Province $province, User $user)
+    {
+        $this->provinceModel = $province;
+        $this->userModel     = $user;
+    }
 
     public function profile(Request $request){
-        if($request->method() == "POST"){
+            $user      = Auth::user();
+            $provinces = $this->provinceModel->pluck("province_name","province_code");
+            return view("dashboard.profile",compact("user","provinces"));
+    }
 
-        }else{
-            $user = Auth::user();
-            return view("dashboard.profile",compact("user"));
-        }
+    public function storeProfile(ProfileRequest $request,$userId){
 
+      $user =  $this->userModel->find($userId);
+      $user->update($request->all());
+      return redirect("dashboard/profile")->with(["status"=>"success","message"=>"อัพเดตข้อมูลสำเร็จ"]);
     }
 }
